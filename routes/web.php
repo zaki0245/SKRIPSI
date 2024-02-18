0ac90202377b0dd1b879881c0822a7d22eb6b553
zaki <muhammad.zaki0245@gmail.com>
2024-02-18 18:27:31 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SPKSahamController;
 
 Route::get('/data', [SPKSahamController::class, 'index'])->name('data.index');
-// Route::get('/perhitungan', [SPKSahamController::class, 'perhitungan'])->name('perhitungan');
-// Route::get('/perhitungan', [SPKSahamController::class, 'hitung'])->name('perhitungan.hitung');
 Route::get('/perhitungan', [SPKSahamController::class, 'hitung'])->name('perhitungan');
 Route::get('/evaluasi', [SPKSahamController::class, 'evaluasi'])->name('evaluasi');
